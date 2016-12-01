0e71d77d7747e8b94567d5dfd60d50544a3cc029
Yuki MIZUNO <mizuyuu0904@gmail.com>
2016-12-02 00:14:22 +0900

    Fix button not found in my self post page

--- src/index.jsx
+++ src/index.jsx
@@ -6,31 +6,34 @@ import {Store, INITIAL_STATE, LIKED_STATE} from "./Store"
 window.React = React
 
 // Like button
-const likeButton = document.querySelector(".LikeButton > button")
-likeButton.addEventListener("click", (eve) => {
-    Store.dispatch({type: LIKED_STATE, data: !likeButton.classList.contains("liked")})
-})
-Store.dispatch({type: LIKED_STATE, data: likeButton.classList.contains("liked")})
-Store.subscribe(() => {
-    const {disliked} = Store.getState()
-    if (disliked) {
-        likeButton.classList.add("disabled")
-    } else {
-        likeButton.classList.remove("disabled")
-    }
-})
+const likeButton = document.querySelector(".ArticleMainHeader .LikeButton > button")
+if (likeButton) { // not my self post
+    likeButton.addEventListener("click", (eve) => {
+        Store.dispatch({type: LIKED_STATE, data: !likeButton.classList.contains("liked")})
+    })
+    Store.dispatch({type: LIKED_STATE, data: likeButton.classList.contains("liked")})
+    Store.subscribe(() => {
+        const {disliked} = Store.getState()
+        if (disliked) {
+            likeButton.classList.add("disabled")
+        } else {
+            likeButton.classList.remove("disabled")
+        }
+    })
 
-// Button
-const buttonWrapper = document.createElement("div")
-const userList = document.querySelector(".list-inline.ArticleMainHeader__users")
 
-buttonWrapper.id = "dislike-button"
-userList.parentNode.insertBefore(buttonWrapper, userList)
+    // Button
+    const buttonWrapper = document.createElement("div")
+    const userList = document.querySelector(".list-inline.ArticleMainHeader__users")
 
-ReactDOM.render(
-    <Button />,
-    document.getElementById("dislike-button")
-)
+    buttonWrapper.id = "dislike-button"
+    userList.parentNode.insertBefore(buttonWrapper, userList)
+
+    ReactDOM.render(
+        <Button />,
+        document.getElementById("dislike-button")
+    )
+}
 
 
 // Label
