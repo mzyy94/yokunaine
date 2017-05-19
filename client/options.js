eb28d688910f1f8dbbf40846dc5e53cdc4efeb80
Yuki MIZUNO <mizuyuu0904@gmail.com>
2017-05-19 21:58:58 +0900

    Improve option page visualization

--- client/options.js
+++ client/options.js
@@ -1,20 +1,20 @@
-new Vue({ // eslint-disable-line no-new
-  el: 'section',
-  data: {
-    savedUrl: 'https://service.yokunaine.mzyy94.com/api/v1',
-    url: 'https://service.yokunaine.mzyy94.com/api/v1',
-    token: '',
-    report: false
-  },
-  created () {
-    window.chrome.storage.sync.get(['service_uri', 'token', 'report'], ({service_uri: uri, token, report}) => {
-      if (uri === undefined) {
+window.chrome.storage.sync.get(['service_uri', 'token', 'report'], ({ service_uri: url, token, report }) => {
+  new Vue({ // eslint-disable-line no-new
+    el: 'main',
+    data: {
+      savedUrl: 'https://service.yokunaine.mzyy94.com/api/v1',
+      url,
+      token,
+      report
+    },
+    created () {
+      if (this.url === undefined) {
         window.chrome.storage.sync.set({service_uri: this.savedUrl}, () => {})
+        this.url = this.savedUrl
       } else {
-        this.url = uri
-        this.savedUrl = uri
+        this.savedUrl = this.url
       }
-      if (token === undefined) {
+      if (this.token === undefined) {
         const token = new window.URLSearchParams(window.location.search).get('token')
         if (token) {
           window.chrome.storage.sync.set({token}, () => {
@@ -22,43 +22,40 @@ new Vue({ // eslint-disable-line no-new
             this.token = token
           })
         }
-      } else {
-        this.token = token
       }
-      this.report = report
-    })
-  },
-  watch: {
-    report: (newVal) => {
-      window.chrome.storage.sync.set({report: newVal}, () => {})
+    },
+    watch: {
+      report: (newVal) => {
+        window.chrome.storage.sync.set({report: newVal}, () => {})
+      }
+    },
+    methods: {
+      save () {
+        window.chrome.storage.sync.set({service_uri: this.url}, () => {
+          this.savedUrl = this.url
+        })
+      },
+      cancel () {
+        this.url = this.savedUrl
+      },
+      revokeToken () {
+        window.fetch(`${this.savedUrl}/auth/token/${this.token}`, {
+          method: 'DELETE',
+          mode: 'cors'
+        })
+        .then(() => window.chrome.storage.sync.remove('token', () => {
+          this.token = ''
+        }))
+        .catch((e) => {
+          window.alert(e)
+        })
+      },
+      getToken () {
+        const callbackUrl = window.chrome.extension.getURL('options.html')
+        window.chrome.tabs.getCurrent(cur => {
+          window.chrome.tabs.update(cur.id, {url: `${this.savedUrl}/auth?callback=${callbackUrl}`})
+        })
+      }
     }
-  },
-  methods: {
-    save () {
-      window.chrome.storage.sync.set({service_uri: this.url}, () => {
-        this.savedUrl = this.url
-      })
-    },
-    cancel () {
-      this.url = this.savedUrl
-    },
-    revokeToken () {
-      window.fetch(`${this.savedUrl}/auth/token/${this.token}`, {
-        method: 'DELETE',
-        mode: 'cors'
-      })
-      .then(() => window.chrome.storage.sync.remove('token', () => {
-        this.token = ''
-      }))
-      .catch((e) => {
-        window.alert(e)
-      })
-    },
-    getToken () {
-      const callbackUrl = window.chrome.extension.getURL('options.html')
-      window.chrome.tabs.getCurrent(cur => {
-        window.chrome.tabs.update(cur.id, {url: `${this.savedUrl}/auth?callback=${callbackUrl}`})
-      })
-    }
-  }
+  })
 })
